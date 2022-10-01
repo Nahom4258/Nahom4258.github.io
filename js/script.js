a1df9e91d4ae89a4f4f9c87b2e72398e24a57927
Nahom Habtamu <nhabtamu42@gmail.com>
2022-10-01 04:36:40 +0300

    responsive navbar menu toggle

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ const htmlTag = document.getElementsByTagName('html')[0]
 toggleDarkBtn.addEventListener('click', () => {
     htmlTag.classList.toggle('dark')
 })
+
+const responsiveMenuBtn = document.querySelector('#responsive-nav-menu-btn')
+const navMenuContainer = document.querySelector('#nav-items-container')
+
+responsiveMenuBtn.addEventListener('click', () => {
+    navMenuContainer.classList.toggle('hidden')
+})
